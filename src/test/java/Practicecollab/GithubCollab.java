c216ef469d9e5350def0a1ee8deb31ec193247fb
D S Harshitha <D S Harshitha@LAPTOP-62P1RBS3>
2024-01-23 17:42:02 +0530

    TC--01 commited to branch

---
+++ src/test/java/Practicecollab/GithubCollab.java
@@ -0,0 +1,10 @@
+package Practicecollab;
+
+public class GithubCollab {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Collabing");
+	}
+
+}
